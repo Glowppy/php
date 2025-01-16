3a1545f69747578e0c5693b5b40ee17d10e6da6f
Glowppy <113057558+Glowppy@users.noreply.github.com>
2025-01-16 23:06:02 +0100

    Create tabellina.php

---
+++ tabellina.php
@@ -0,0 +1,13 @@
+<?php
+echo "<table style='border: 1px solid black;'>";
+for($a=1;$a<=10;$a++){
+  echo "<tr>;
+    for($b=1;$b<=10;$b++){
+      echo "<td style='border: 1px solid red;'>".$a*$b."</td>";
+        echo $a*$b;
+    }
+    echo "</tr>";
+    echo "<br>";
+    }
+    echo "<table>";
+?>
