34a36ffc8add44eaedf16991964f3742a1182f78
Glowppy <113057558+Glowppy@users.noreply.github.com>
2025-01-16 22:49:13 +0100

    Update funzione_if_2.php

--- funzione_if_2.php
+++ funzione_if_2.php
@@ -1,1 +1,12 @@
+<?php
+$nome = "Mario";
+$cognome = "Rossi";
+$eta = 25;
+$studente = true;
 
+if($eta>=18){
+    echo "<h1>sei maggiorenne</h1>";
+}else{
+    echo "troppo piccolo, torna a casa";
+}
+?>
